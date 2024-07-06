f599ea84d750b39fb8edfa5cb9068c41714ff533
mayanktripathi4u <mayanktripathi4u@gmail.com>
2024-07-06 00:50:24 -0500

    feat: adding bigquery

---
+++ Cloud_SDK/BigQuery_Python/README.md
@@ -0,0 +1,1 @@
+# Loading Data into BigQuery from Cloud Storage Bucket with Python
---
+++ Cloud_SDK/BigQuery_Python/event.py
@@ -0,0 +1,5 @@
+data = {
+    "name": "table-1_data_new_line_delimited_json.json", \
+     "bucket":"bkt_data_source_mt_devops", \
+     "timeCreated": "2023-06-03 15:54:54"\
+}
